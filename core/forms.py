554f02a25ad589bc42c7fa27dcc4ed95a36cc0ef
JoseJesusPalma <superchui32dj@gmail.com>
2025-08-10 02:33:51 -0600

    login no funciono

--- core/forms.py
+++ core/forms.py
@@ -5,3 +5,8 @@ class ReservaForm(forms.ModelForm):
     class Meta:
         model = Reserva
         fields = ['cliente', 'paquete', 'metodo_pago']
+
+
+class log_usu(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
